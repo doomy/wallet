5344f2925451062f410e05744bd822ac6de8e65d
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-01-29 19:15:24 +0100

    Basic presenter

--- lib/App/Wallet.php
+++ lib/App/Wallet.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Base\Controller;
+use Component\Presenter;
+use Template;
 
 class Wallet extends Controller {
 	public function run() {
-		echo "test";
+		$template = new Template('index.tpl.php', array());
+		$presenter = new Presenter($template);
+		echo $presenter->render();
 	}
 }
