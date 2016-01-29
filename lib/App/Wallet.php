542ae08660d5a3139c12adb3c35633c2671488ed
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-01-29 20:05:37 +0100

    Create Form component

--- lib/App/Wallet.php
+++ lib/App/Wallet.php
@@ -5,11 +5,14 @@ namespace App;
 use Base\Controller;
 use Component\Presenter;
 use Component\ComponentFactory;
+use Component\Form;
 use Template;
 
 class Wallet extends Controller {
 	public function run() {
+		$form = ComponentFactory::getComponent(Form::class);
 		$presenter = ComponentFactory::getComponent(Presenter::class);
+		$presenter->addChild($form);
 		echo $presenter->render();
 	}
 }
