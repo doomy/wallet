b841eb74f562be30efda4ce70adf35be16d9448c
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-02-02 17:53:57 +0100

    Add a text input to the form

--- lib/App/Wallet.php
+++ lib/App/Wallet.php
@@ -5,12 +5,14 @@ namespace App;
 use Base\Controller;
 use Component\ComponentFactory;
 use Component\Form;
+use Component\Input\TextInput;
 use Template;
 
 class Wallet extends Controller {
 	public function run() {
 		$form = ComponentFactory::getComponent(Form::class);
-
+		$textInput = ComponentFactory::getComponent(TextInput::class);
+		$form->addChild($textInput);
 		$this->presenter->addChild($form);
 		echo $this->presenter->render();
 	}
