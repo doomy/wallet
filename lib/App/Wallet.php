8f9373ad7923b7947c6614cbf4c14d33f28c106f
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2016-03-07 12:58:17 +0100

    WIP: TracedDates

--- lib/App/Wallet.php
+++ lib/App/Wallet.php
@@ -5,9 +5,11 @@ namespace App;
 use Base\Controller;
 use Module\ExpensesModule;
 use Module\IncomeModule;
+use Model\TracedDateModel;
 
 class Wallet extends Controller {
 	public function run() {
+		$this->updateTracedDates();
 		$expensesModule = new ExpensesModule();
 		$expensesModule->run();
 		$this->presenter->addChild($expensesModule->getComponent());
@@ -16,5 +18,20 @@ class Wallet extends Controller {
 		$this->presenter->addChild($incomeModule->getComponent());
 		$this->presenter->addStylesheet('css/popup.css');
 		echo $this->presenter->render();
+	}
+
+	private function updateTracedDates() {
+		$model = new TracedDateModel(\Environment::get_dbh()->get_mysqli_connection());
+		do {
+			$lastDate = $model->getLastDate();
+			$currentDate = new \DateTime();
+			if ($currentDate->format('Y-m-d') != $lastDate) {
+				$currentDate->add();
+				var_dump($currentDate->format('Y-m-d'));
+			}
+
+			die;
+
+		} while (1 == 2);
 	}
 }
---
+++ lib/Model/TracedDateModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Model;
+
+use Base\Model;
+
+class TracedDateModel extends Model {
+	public function getLastDate() {
+		$result = $this->mysqli->query("SELECT MAX(traced_date) AS lastDate FROM t_traced_date");
+		$row = $result->fetch_object();
+		return $row->lastDate;
+	}
+
+	public function insertDate($date) {
+		$this->mysqli->query("INSERT INTO t_traced_date (traced_date) VALUES '$date';");
+	}
+}
